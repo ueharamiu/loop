0ec910bd72e707dc46d49a98ca16f5f9e8937113
ueharamiu <ueharamiu66@gmail.com>
2020-04-18 20:43:55 +0900

    while と for の練習

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
+
+
 <?php
-  if():
-    while():
 
-      endwhile;
-    else:
+$i = 0;
+while($i <= 20) {
+  echo $i;
+  $i = $i + 2;
+}
 
-    endif;
+echo '<br>';
+
+$i = 0;
+while($i < 5) {
+  echo $i;
+  $i++;
+  // code...
+}
+
+echo '<br>';
+
+for($i = 0; $i <= 10; $i++) {
+  echo $i;
+}
+
+echo '<br>';
+
+for($i = 2,$j = 100; $i <= 20; $i*2,$j*2) {
+  echo $i;
+  $i = $i * 2;
+  echo $j;
+}
+
 ?>
