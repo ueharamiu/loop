4bba6b8ab5c9fd65f36a6b3c7f959001ebf358f8
ueharamiu <ueharamiu66@gmail.com>
2020-04-22 21:08:03 +0900

    php 練習

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 
 
 <?php
+// テスト
+  echo "Hellow World";
+  echo '<br>';
+  echo '<br>';
+$i = 200;
+  print( "このパンは" . $i . "円です" );
 
+echo '<br>';
+echo '<br>';
+define( "TITLE", "オレンジ" );
+define( "TITLE", "みかん" );
+define( "TAX", 2.2 );
+echo TITLE . "は" . 300 * TAX . "円です";
+
+echo '<br>';
+echo '<br>';
 $i = 0;
 while($i <= 20) {
   echo $i;
@@ -29,5 +44,34 @@ for($i = 2; $i <= 20; $i *2) {
   echo $i;
   $i = $i * 2;
   echo '<br>';
+}
+echo '<br>';
+
+$i = array( 300, 400, 500,);
+  echo $i[0];
+  echo $i[1];
+  echo $i[2];
+
+echo '<br>';
+
+$i = array( 300, 400, 500,);
+for($j = 0; $j < 3; $j++ ) {
+  echo $j;
+}
+
+echo '<br>';
+echo '<br>';
+
+$i = array( 315, 420, 520, 100, 50, 3, "田中");
+for($j = 0; $j < 7; $j++ ) {
+  echo $i[$j] . '<br>';
+}
+
+echo '<br>';
+echo '<br>';
+
+$i = array( "田中", "山田", "佐藤");
+for($j = 0; $j < 4; $j++ ) {
+  echo $i[$j] . '<br>';
 }
 ?>
